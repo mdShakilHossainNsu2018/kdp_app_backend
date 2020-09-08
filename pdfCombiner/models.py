e27c939e0811cdc3276cf89e4d478d7c8957b9e5
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-09-08 12:27:18 +0600

    pdf file name unique false

--- pdfCombiner/models.py
+++ pdfCombiner/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Pdf(models.Model):
     ip = models.GenericIPAddressField(blank=True, null=True)
-    pdf_file_name = models.CharField(max_length=40, unique=True)
+    pdf_file_name = models.CharField(max_length=40)
     pdf_file = models.FileField(upload_to='pdf/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
