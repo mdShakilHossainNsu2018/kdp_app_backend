7ff736d7c423899a484d66d954b4aa71ca2ac0a9
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-09-09 11:33:03 +0600

    total_page added

--- pdfCombiner/models.py
+++ pdfCombiner/models.py
@@ -6,6 +6,7 @@ class Pdf(models.Model):
     ip = models.GenericIPAddressField(blank=True, null=True)
     pdf_file_name = models.CharField(max_length=40)
     pdf_file = models.FileField(upload_to='pdf/', blank=True, null=True)
+    total_pages = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
