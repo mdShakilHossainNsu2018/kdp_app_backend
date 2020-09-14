4b57979af13e1ccfabc34c330f5300df0a780c68
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-09-14 17:12:26 +0600

    fix v1

--- categories/models.py
+++ categories/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
     category_name = models.CharField(max_length=250)
     url = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- categories/serializers.py
+++ categories/serializers.py
@@ -3,10 +3,28 @@ from rest_framework import serializers
 from categories.models import Category
 
 
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     # subcategories = serializers.ManyRelatedField()
     patent_category_name = serializers.CharField(read_only=True, source="parent_category.category_name")
+    # subcategories = SubCategorySerializer()
 
     class Meta:
         model = Category
-        fields = ['id', 'category_name', 'parent_category', 'parent_category_id', 'patent_category_name', 'url', 'created_at', 'updated_at']
+        fields = ['id', 'category_name', 'parent_category', 'subcategories', 'parent_category_id', 'patent_category_name', 'url',
+                  'created_at', 'updated_at']
+
+    def get_fields(self):
+        fields = super(CategorySerializer, self).get_fields()
+        fields['subcategories'] = CategorySerializer(many=True)
+        return fields
+
+        # def get_related_field(self, model_field):
+        #     # Handles initializing the `subcategories` field
+        #     return CategorySerializer()
+# CategorySerializer.base_fields['subcategories'] = CategorySerializer()
