e24ed817f1522ec29b915370d940b4c435e67a39
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-09-16 20:32:14 +0600

    ip address added

--- kdp_app_backend/settings.py
+++ kdp_app_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = ')nie8dez3tn8fhve=8q1#l9z2&oz7d^y@gncxd*b2=7f23mxo)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['164.90.238.120']
 
 X_FRAME_OPTIONS = 'ALLOW-FROM http://127.0.0.1:8000/'
 
