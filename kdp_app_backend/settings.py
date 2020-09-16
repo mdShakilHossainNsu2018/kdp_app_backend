f83b22aa3b45886f61bdd92da450e5f890feabce
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-09-16 20:37:41 +0600

    database setup

--- kdp_app_backend/settings.py
+++ kdp_app_backend/settings.py
@@ -101,10 +101,21 @@ WSGI_APPLICATION = 'kdp_app_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kdp_app',
+        'USER': 'kdp_app',
+        'PASSWORD': 'codeforsolutions.com',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -142,10 +153,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.dirname(os.path.join(BASE_DIR, 'media'))
-STATIC_ROOT = os.path.dirname(os.path.join(BASE_DIR, 'static'))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Todo: heroku prod settings
 # django_heroku.settings(locals())
