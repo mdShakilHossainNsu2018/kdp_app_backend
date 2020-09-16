e5c5aa201d36b66566b7ce53303334c7f281ed8a
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-09-16 23:09:07 +0600

    v3

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,7 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Category
 from .serializers import CategorySerializer
@@ -9,16 +12,16 @@ class CategoryModelViewSets(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
-    # @method_decorator(cache_page(60*5)) # cache for 5 minutes
-    # def list(self, request, format=None):
-    #     queryset = self.filter_queryset(self.get_queryset())
-    #
-    #     page = self.paginate_queryset(queryset)
-    #     if page is not None:
-    #         serializer = self.get_serializer(page, many=True)
-    #         return self.get_paginated_response(serializer.data)
-    #
-    #     serializer = self.get_serializer(queryset, many=True)
-    #     return Response(serializer.data)
+    @method_decorator(cache_page(9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999*9999999999999999999999999)) # cache for 5 minutes
+    def list(self, request, format=None):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
